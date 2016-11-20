e9c3aa53243465fea9eb7ffca5c5b44c1a9c5780
[null] <null@disasterarea.net>
2016-11-20 23:14:12 +1100

    Download/extract save state as user steam, so we can overwrite it later

--- puppet/modules/profile/manifests/dontstarve.pp
+++ puppet/modules/profile/manifests/dontstarve.pp
@@ -9,9 +9,11 @@ class profile::dontstarve {
   exec { "aws s3 cp s3://dst.aws.disasterarea.ninja/save.tar.xz .":
     cwd => "/home/steam",
     creates => "/home/steam/save.tar.xz",
+    user => "steam",
   } -> exec { "tar xJf save.tar.xz":
     cwd => "/home/steam/",
     creates => "/home/steam/.klei",
+    user => "steam",
     before => Class['steamcmd::dontstarve'],
   }
 
